6484ac1d4fdefc3830fd0a03536289de57535464
Kevin Castro Cruz <kevinclap@Kevins-MacBook-Pro.local>
2018-12-25 17:31:12 -0400

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -2,23 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './components/Header';
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Header/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
-    );
+    )
   }
 }
 
---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+import { Navbar } from 'react-bootstrap';
+
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+     <Navbar inverse collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="http://svc.metrotransit.org/" target="_blank" rel="noopener noreferrer">Metro Transit</a>
+        </Navbar.Brand>
+      <Navbar.Toggle />
+     </Navbar.Header>
+  </Navbar>
+    </div>
+  )
+}
+
+export default Header;
